d85332ffe121848e36804f5cd044628b97a89021
omaimarababaa <omaima.rababaa@gmail.com>
2022-12-31 03:43:51 +0300

    get data from json

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,17 +1,21 @@
-
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-  export class HomeComponent implements OnInit {
-    
-  
-    public constructor() {}
-  
-    public ngOnInit(): void {
-   
-    }
+export class HomeComponent implements OnInit {
+  public companyInfo: any;
+
+  public constructor(private http: HttpClient) {}
+
+  public ngOnInit(): void {
+    const url: string = '../../assets/fullData/companies.json';
+    this.http.get(url).subscribe((response) => {
+      this.companyInfo = response;
+      console.log(this.companyInfo + 'hhhh');
+    });
   }
+}
